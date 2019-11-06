8c7184a00abcdcc66ed5e76d01acfd398aa37148
Vladimir Kurbatov <warkb@yandex.ru>
2019-11-06 16:33:59 +0400

    Insert sort

--- MYComputerScience/Program.cs
+++ MYComputerScience/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MYComputerScience.Sorts;
+using System;
 
 
 namespace MYComputerScience
@@ -10,14 +11,17 @@ namespace MYComputerScience
         {
             BubbleSort bubble = new BubbleSort();
             SelectionSort selection = new SelectionSort();
-
+            InsertionSort insertion = new InsertionSort();
 
 
             Console.WriteLine("Bubble Sort");
            // bubble.sort();
 
             Console.WriteLine("Selection Sort");
-            selection.selectionSort();
+            //selection.selectionSort();
+
+            Console.WriteLine("Insertion Sort");
+            insertion.insertionSort();
 
 
         }
