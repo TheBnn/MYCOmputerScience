cd141bd5ac80b3e6ade2de1cbd3e79b9ac91d970
Vladimir Kurbatov <warkb@yandex.ru>
2019-11-11 12:15:33 +0400

    merge sort TODO

--- MYComputerScience/Program.cs
+++ MYComputerScience/Program.cs
@@ -12,16 +12,21 @@ namespace MYComputerScience
             BubbleSort bubble = new BubbleSort();
             SelectionSort selection = new SelectionSort();
             InsertionSort insertion = new InsertionSort();
+            MergeSort merge = new MergeSort();
 
+            //Console.WriteLine("Bubble Sort");
+            // bubble.sort();
 
-            Console.WriteLine("Bubble Sort");
-           // bubble.sort();
-
-            Console.WriteLine("Selection Sort");
+            //Console.WriteLine("Selection Sort");
             //selection.selectionSort();
 
-            Console.WriteLine("Insertion Sort");
-            insertion.insertionSort();
+            //Console.WriteLine("Insertion Sort");
+            //insertion.insertionSort();
+
+
+            //Console.WriteLine("Merge Sort");
+            //Console.WriteLine(MergeSort.mergeSort(arr));
+
 
 
         }
